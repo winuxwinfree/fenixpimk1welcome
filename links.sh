e8768dd7cd73b3fff993307c6eb1d231ef2d2616
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-07-11 16:56:42 +0200

    Delete links.sh

--- links.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-chromium-browser --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36" --app=file:///home/no10/links/fenixpimk1links.html --window-size=1024,768
-
